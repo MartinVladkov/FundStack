b32be1db5f036df05b52064d3c772a222795ff33
MartinVladkov <martin_vladkov@abv.bg>
2022-03-22 22:00:34 +0200

    Work on fear and greed index

--- FundStack/Controllers/HomeController.cs
+++ FundStack/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace FundStack.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult FearAndGreedIndex()
+        {
+            return View();
+        }
     }
 }
