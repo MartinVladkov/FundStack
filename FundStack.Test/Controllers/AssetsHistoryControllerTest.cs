cf0963f020249051ee24fcdd9dd01a454c35e14d
MartinVladkov <martin_vladkov@abv.bg>
2022-04-08 22:06:27 +0300

    Unit tests of Assets History Controller

---
+++ FundStack.Test/Controllers/AssetsHistoryControllerTest.cs
@@ -0,0 +1,80 @@
+﻿using FundStack.Controllers;
+using FundStack.Services.AssetsHistory;
+using FundStack.Test.Mocks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FundStack.Test.Controllers
+{
+    public class AssetsHistoryControllerTest
+    {
+        //Helper Classes
+        private AssetsHistoryController ArrangeTest(string userId)
+        {
+            var assetHistoryService = AssetHistoryServiceMock.Instance;
+            var assetsHistoryController = new AssetsHistoryController(assetHistoryService);
+
+            assetsHistoryController.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = MockUser(userId) }
+            };
+
+            return assetsHistoryController;
+        }
+
+        private ClaimsPrincipal MockUser(string userId)
+        {
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                   new Claim(ClaimTypes.Name, "example name"),
+                   new Claim(ClaimTypes.NameIdentifier, userId),
+                   new Claim("custom-claim", "example claim value"),
+            }, "mock"));
+
+            return user;
+        }
+
+        //Tests of AllHistory()
+        [Fact]
+        public void AllHistoryShouldReturnView()
+        {
+            //Arrange
+            var userId = "valid";
+            var assetsHistoryController = ArrangeTest(userId);
+
+            //Act
+            var result = assetsHistoryController.AllHistory();
+
+            //Assert
+            Assert.NotNull(result);
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            var model = viewResult.Model;
+            Assert.IsType<List<AssetHistoryServiceModel>>(model);
+        }
+
+        //Tests of Export()
+        [Fact]
+        public void ExportShouldReturnFile()
+        {
+            //Arrange
+            var userId = "valid";
+            var assetsHistoryController = ArrangeTest(userId);
+
+            //Act
+            var result = assetsHistoryController.Export();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<FileContentResult>(result);
+        }
+
+    }
+}
---
+++ FundStack.Test/Mocks/AssetHistoryServiceMock.cs
@@ -0,0 +1,31 @@
+﻿using FundStack.Services.AssetsHistory;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundStack.Test.Mocks
+{
+    public static class AssetHistoryServiceMock
+    {
+        public static IAssetHistoryService Instance
+        {
+            get
+            {
+                var assetHistoryServiceMock = new Mock<IAssetHistoryService>();
+
+                assetHistoryServiceMock
+                    .Setup(a => a.AllHistory("valid"))
+                    .Returns(new List<AssetHistoryServiceModel>
+                    {
+                        new AssetHistoryServiceModel { Id = 1, Name = "test1" },
+                        new AssetHistoryServiceModel { Id = 2, Name = "test2" }
+                    });
+
+                return assetHistoryServiceMock.Object;
+            }
+        }
+    }
+}
