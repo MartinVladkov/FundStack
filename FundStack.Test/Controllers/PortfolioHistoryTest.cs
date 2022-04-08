1640e99f1f883d4e4e23822b48279f953b400657
MartinVladkov <martin_vladkov@abv.bg>
2022-04-08 21:35:26 +0300

    Unit test Portfolio History Controller

---
+++ FundStack.Test/Controllers/PortfolioHistoryTest.cs
@@ -0,0 +1,53 @@
+﻿using FundStack.Controllers;
+using FundStack.Models.PortfolioHistory;
+using FundStack.Test.Mocks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FundStack.Test.Controllers
+{
+    public class PortfolioHistoryTest
+    {
+        private ClaimsPrincipal MockUser(string userId)
+        {
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                   new Claim(ClaimTypes.Name, "example name"),
+                   new Claim(ClaimTypes.NameIdentifier, userId),
+                   new Claim("custom-claim", "example claim value"),
+            }, "mock"));
+
+            return user;
+        }
+
+        [Fact]
+        public void TotalValue()
+        {
+            //Arrange
+            var portfolioHistoryMock = PortfolioHistoryServiceMock.Instance;
+            var assetsController = new PortfolioHistoryController(portfolioHistoryMock);
+
+            assetsController.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = MockUser("valid") }
+            };
+
+            //Act
+            var result = assetsController.TotalValue();
+
+            //Assert
+            Assert.NotNull(result);
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            var model = viewResult.Model;
+            Assert.IsType<PortfolioHistoryViewModel>(model);
+        }
+    }
+}
---
+++ FundStack.Test/Mocks/PortfolioHistoryServiceMock.cs
@@ -0,0 +1,33 @@
+﻿using FundStack.Services.PortfoliosHistory;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundStack.Test.Mocks
+{
+    public static class PortfolioHistoryServiceMock
+    {
+        public static IPortfolioHistoryService Instance
+        {
+            get
+            {
+                var portfolioHistoryServiceMock = new Mock<IPortfolioHistoryService>();
+
+                //TotalValue()
+                portfolioHistoryServiceMock
+                    .Setup(p => p.TotalValue("valid"))
+                    .Returns(new List<PortfolioHistoryServiceModel>
+                    {
+                       new PortfolioHistoryServiceModel { PortfolioId = "1",
+                        PortfolioValue = 250,
+                        SnapshotDate = DateTime.UtcNow }
+                    });
+
+                return portfolioHistoryServiceMock.Object;
+            }
+        }
+    }
+}
