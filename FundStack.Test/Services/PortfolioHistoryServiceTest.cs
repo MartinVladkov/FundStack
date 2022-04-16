969183e9b7e8a18c0db43320e95197b74f71388b
MartinVladkov <martin_vladkov@abv.bg>
2022-04-16 19:20:08 +0300

    Unit tests for Portfolio History Service

--- FundStack.Test/Services/PortfolioHistoryServiceTest.cs
+++ FundStack.Test/Services/PortfolioHistoryServiceTest.cs
@@ -1,4 +1,5 @@
 ﻿using FundStack.Data.Models;
+using FundStack.Services.PortfoliosHistory;
 using FundStack.Test.Mocks;
 using System;
 using System.Collections.Generic;
@@ -25,14 +26,77 @@ namespace FundStack.Test.Services
                 Portfolio = new Portfolio()
                 {
                     UserId = userId,
-                    
+                    PortfolioHistory = new List<PortfolioHistory>()
+                    {
+                       new PortfolioHistory { 
+                           SnapshotDate = Convert.ToDateTime("03/22/2022"),
+                           PortfolioValue = 1500
+                       },
+                       new PortfolioHistory {
+                           SnapshotDate = Convert.ToDateTime("03/23/2022"),
+                           PortfolioValue = 1600
+                       },
+                       new PortfolioHistory {
+                           SnapshotDate = Convert.ToDateTime("03/24/2022"),
+                           PortfolioValue = 1550
+                       },
+                    }
                 }
             });
 
-            //Act
+            data.SaveChanges();
+            var portfolioHistoryService = new PortfolioHistoryService(data);
 
+            //Act
+            var result = portfolioHistoryService.TotalValue(userId);
 
             //Assert
+            Assert.NotNull(result);
+            Assert.IsType<List<PortfolioHistoryServiceModel>>(result);
+            Assert.Equal(3, result.Count());
+            Assert.Equal(1500, result.First().PortfolioValue);
+        }
+
+        [Fact]
+        public void RecordValuesSavesCurrentTotalValueOfPortfolio()
+        {
+            //Arrange
+            using var data = DatabaseMock.Instance;
+            var userId = "testId";
+
+            data.Users.Add(new User()
+            {
+                Id = userId,
+                Portfolio = new Portfolio()
+                {
+                    UserId = userId,
+                    PortfolioHistory = new List<PortfolioHistory>()
+                    {
+                       new PortfolioHistory {
+                           SnapshotDate = Convert.ToDateTime("03/22/2022"),
+                           PortfolioValue = 1500
+                       },
+                    },
+                    TotalValue = 1550
+                }
+            });
+
+            data.SaveChanges();
+            var user = data.Users
+                .Where(u => u.Id == userId)
+                .FirstOrDefault();
+            var history = user.Portfolio.PortfolioHistory;
+
+            var portfolioHistoryService = new PortfolioHistoryService(data);
+
+            //Act
+            portfolioHistoryService.RecordPorfoltioValue();
+
+            //Assert
+            Assert.NotNull(history);
+            Assert.Equal(2, history.Count());
+            Assert.Equal(1500, history.First().PortfolioValue);
+            Assert.Equal(1550, history.Last().PortfolioValue);
         }
     }
 }
